8a4f414475b55b8d23ed357ecc8b879ef875c1c5
newberg85 <bergviana12345@gmail.com>
2024-02-02 11:10:35 -0300

    Operadores de Incremento e Decremento

---
+++ src/Aula03IncrementoEDecremento.java
@@ -0,0 +1,19 @@
+public class Aula03IncrementoEDecremento {
+
+    public static void main(String[] args) {
+        int num1, num2;
+
+        num1 = 6;
+        num2 = 6;
+
+        System.out.println(num1);
+        System.out.println(num2);
+
+        num1++; //num1 = num1 + 1
+        ++num2; //num2 = num2 + 2
+
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+
+}
