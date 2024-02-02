96c8de9c9753a4b01d8328d871b8cbba1bff6546
newberg85 <bergviana12345@gmail.com>
2024-02-02 02:38:22 -0300

    Exercicio 01

---
+++ src/Exercicio01TiposPrimitivos.java
@@ -0,0 +1,14 @@
+public class Exercicio01TiposPrimitivos {
+    
+    public static void main(String[] args) {
+        double nota1 = 9.0;
+        double nota2 = 10.0;
+        double nota3 = 8.0;
+        double media;
+
+        media = (nota1 + nota2 + nota3) / 3;
+
+        System.out.println(media);
+    }
+
+}
