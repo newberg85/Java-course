e5e6ef06cda9962bbdc117d9cd0e6723bd640dcb
newberg85 <bergviana12345@gmail.com>
2024-02-02 11:38:35 -0300

    Operadors Aritmeticos de atribuição e Operadores relacionais

---
+++ src/Aula04AritmeticosDeAtribuicaoERelacionais.java
@@ -0,0 +1,42 @@
+public class Aula04AritmeticosDeAtribuicaoERelacionais {
+
+    // OPERADORES ARITMETICOS COM ATRIBUÇÃO
+
+    /* 
+     += : Atribuição aditiva
+     -= : Atribuição subtrativa
+     *= : Atribuição de multiplicação
+     /= : Atribuição de divisão
+     %= : Atribuição de modulo
+    */ 
+
+    /* 
+     == : Igual
+     != : Diferente
+     < : Menor
+     > : Maior
+     >= : Maior ou igual
+     <= : Menor ou igual
+    */
+    
+    public static void main(String[] args) {
+        int numero;
+
+        numero = 6;
+        //numero = numero++; (numero + 1)
+        numero += 2; // numero = numero + 2;
+        System.out.println(numero);
+        numero -= 2;
+        System.out.println(numero); // numero = numero - 2;
+
+
+        int numero2;
+
+        numero2 = 7;
+
+        System.out.println(numero2 > 7); // False
+        System.out.println(numero >= 7); // True
+        System.out.println(numero2 < 5); // False
+    }
+
+}
