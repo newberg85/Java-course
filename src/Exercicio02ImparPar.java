617d735eccb01b7710c2e8a8547d0cdf4ab7de9b
newberg85 <bergviana12345@gmail.com>
2024-02-02 02:40:28 -0300

    Exercicio Impar ou par

---
+++ src/Exercicio02ImparPar.java
@@ -0,0 +1,13 @@
+public class Exercicio02ImparPar {
+    
+    public static void main(String[] args) {
+        int valor = 2;
+
+        if(valor % 2 == 0){
+            System.out.println("Esse número é par");
+        } else {
+            System.out.println("Esse número é impar");
+        }
+    }
+    
+}
