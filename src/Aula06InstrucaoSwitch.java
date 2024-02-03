0e0899819ff5f581218f6436c14ba8669ec41e59
newberg85 <bergviana12345@gmail.com>
2024-02-03 02:10:22 -0300

    Aula 06 Estruturas de decisão Switch

---
+++ src/Aula06InstrucaoSwitch.java
@@ -0,0 +1,18 @@
+public class Aula06InstrucaoSwitch {
+
+    public static void main(String[] args) {
+        int numero = 1;
+
+        switch (numero) {
+            case 1:
+                System.out.println("Você digitou 1");
+                break;
+            case 2:
+                System.out.println("Você digitou 2");
+                break;
+            default:
+                System.out.println("Você digitou um valor invalido");
+        }
+    }
+
+}
