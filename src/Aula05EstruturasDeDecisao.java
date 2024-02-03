848fc12fc6dee0ae5a21336f7ca5c6e0f5aa5d72
newberg85 <bergviana12345@gmail.com>
2024-02-03 01:39:18 -0300

    Aula 04 Estruturas de decisaão

---
+++ src/Aula05EstruturasDeDecisao.java
@@ -0,0 +1,15 @@
+public class Aula05EstruturasDeDecisao {
+
+    public static void main(String[] args) {
+        int valor = 8;
+
+        if(valor == 8){
+            System.out.println("O valor é igual a 8");
+        } else if(valor > 8){
+            System.out.println("O valor é maior que 8");
+        } else {
+            System.out.println("O valor é menor que 8");
+        }
+    }
+
+}
