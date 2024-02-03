8b4cb5ea5be74b50d2f10a80902a0dfb9c7daef1
newberg85 <bergviana12345@gmail.com>
2024-02-03 01:59:45 -0300

    Exercicio com estrutura de decisão if

---
+++ src/Exercicio03EstruturasDeDecisaoIf.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Exercicio03EstruturasDeDecisaoIf {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in); // digitar via teclado
+
+        double nota1, nota2, nota3, media;
+
+        System.out.println("Digite o valor da nota 1:");
+        nota1 = input.nextDouble();
+
+        System.out.println("Digite o valor da nota 2:");
+        nota2 = input.nextDouble();
+
+        System.out.println("Digite o valor da nota 3:");
+        nota3 = input.nextDouble();
+
+        media = (nota1 + nota2 + nota3) / 3;
+
+        if (media >= 7) {
+            System.out.println("O aluno está aprovado e a média dele é " + media);
+        } else if (media < 4) {
+            System.out.println("Esse aluno está reprovado e a media dele é " + media);
+        } else {
+            System.out.println("O aluno está em recuperação final " + media);
+        }
+    }
+}
