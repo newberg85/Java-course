86093878a7e43b47ea6a4af42e7c944560a7528a
newberg85 <bergviana12345@gmail.com>
2024-02-04 02:06:00 -0300

    Aula 08 istrução de loop Do while

---
+++ src/Aula08InstrucoesDeLoopDoWhile.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Aula08InstrucoesDeLoopDoWhile {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int op;
+
+        do {
+            System.out.println("--------------");
+            System.out.println("Menu principal");
+            System.out.println("--------------");
+            System.out.println("Selecione uma das opções abaixo");
+            System.out.println("1 - Aluno");
+            System.out.println("2 - Professor");
+            System.out.println("3 - Coordenador");
+            System.out.println("0 - Sair");
+
+            System.out.println("Qual opção deseja?");
+            op = teclado.nextInt();
+
+            if (op == 0) {
+                break;
+            }
+
+            switch (op) {
+                case 1:
+                    System.out.println("Você selecionou a opção de aluno");
+                    break;
+                case 2:
+                    System.out.println("Você selecionou a opção de Professor");
+                    break;
+                case 3: 
+                    System.out.println("Você selecionou a opção de coodernador");
+                    break;
+                default:
+                    break;
+            }
+        } while (true);
+
+        System.out.println("Fim");
+    }
+
+}
