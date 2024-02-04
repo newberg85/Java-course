caf7b77ff0059ff3dbca13cbe1781dd998f66304
newberg85 <bergviana12345@gmail.com>
2024-02-04 18:10:19 -0300

    Aula 11 Array

---
+++ src/Aula11Array.java
@@ -0,0 +1,16 @@
+public class Aula11Array {
+
+    public static void main(String[] args) {
+        // int nota1, nota2, nota3;
+        int[] nota = new int[3];
+
+        nota[0] = 9;
+        nota[1] = 10;
+        nota[2] = 8;
+
+        System.out.println(nota[0]);
+        System.out.println(nota[1]);
+        System.out.println(nota[2]);
+    }
+
+}
