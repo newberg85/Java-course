4d5cd293b9c40085f832633afd27ec8c3b88425e
newberg85 <bergviana12345@gmail.com>
2024-02-04 02:11:27 -0300

    Aula 09 istrução de loop For

---
+++ src/Aula09InstrucoesDeLoopFor.java
@@ -0,0 +1,10 @@
+public class Aula09InstrucoesDeLoopFor {
+
+    public static void main(String[] args) {
+        // int i;
+        for(int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
