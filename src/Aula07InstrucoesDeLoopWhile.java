42fb989d65ed4feb20cca75ee8e72b4f0fe0b96c
newberg85 <bergviana12345@gmail.com>
2024-02-04 01:51:36 -0300

    Aula 07 istrução de loop while

---
+++ src/Aula07InstrucoesDeLoopWhile.java
@@ -0,0 +1,12 @@
+public class Aula07InstrucoesDeLoopWhile {
+
+    public static void main(String[] args) {
+        int numero = 1;
+
+        while (numero <= 5) {
+            System.out.println("Numero");
+            numero++;
+        }
+    }
+
+}
