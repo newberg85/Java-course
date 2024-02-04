f6e58d5fbb4b4b43ef1f150fd8030244341e019e
newberg85 <bergviana12345@gmail.com>
2024-02-04 11:27:28 -0300

    Exercicio com String

---
+++ src/Exercicio05String.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Exercicio05String {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        double nota1, nota2, nota3, media;
+        String nome;
+        
+        for(int i = 0; i < 3; i++) {
+            System.out.println("Nome do aluno: ");
+            nome = teclado.nextLine();
+            System.out.println("Digite a nota 1: ");
+            nota1 = teclado.nextDouble();
+            System.out.println("Digite a nota 2: ");
+            nota2 = teclado.nextDouble();
+            System.out.println("Digite a nota 3: ");
+            nota3 = teclado.nextDouble();
+            teclado.nextLine();
+            media = (nota1 + nota2 + nota3) / 3;
+            System.out.println("A media de " + nome + " é " + media);
+        }
+    }
+
+}
