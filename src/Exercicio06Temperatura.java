7e6091149cc7b89f089fa5859ad05db83b8d23bc
newberg85 <bergviana12345@gmail.com>
2024-02-05 00:13:26 -0300

    Exercicio coverter temperatura

--- src/Exercicio06Temperatura.java
+++ src/Exercicio06Temperatura.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
+
 public class Exercicio06Temperatura {
-    
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        final double ajuste = 32; // final é usado para constantes 
+        final double fator = 5 / 9.0;
+        double grausF;
+        double grausC;
+
+        System.out.println("Digite a quantidade de Graus em fahrenheit: ");
+        grausF = input.nextInt();
+        grausC = (grausF - ajuste) * fator;
+        System.out.println("O valor de ºF" + grausF + " é ºC" + grausC );
+    }
+
 }
