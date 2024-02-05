33e478f2b88e561332f3beb49059247580c517de
newberg85 <bergviana12345@gmail.com>
2024-02-05 01:20:06 -0300

    Aula 13 Imports

---
+++ src/Aula13Imports.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+//import javax.swing.JButton;
+
+public class Aula13Imports {
+
+    public static void main(String[] args) {
+        Date d = new Date();
+        System.out.println(d);
+
+        //JButton botao = new JButton();
+    }
+
+}
