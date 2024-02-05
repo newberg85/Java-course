c9c9cd4006c0b42e0fdebb1c27d335987a1faa2b
newberg85 <bergviana12345@gmail.com>
2024-02-05 01:00:50 -0300

    Aula 12 notação ponto

---
+++ src/Aula12NotacaoPonto.java
@@ -0,0 +1,15 @@
+public class Aula12NotacaoPonto {
+
+    public static void main(String[] args) {
+        String s = "Bom dia";
+        s = s.replace("dia", "boa noite");
+        s = s.toUpperCase();
+        s = s.concat("!!!");
+
+
+        System.out.println(s);
+
+        System.out.println("Berg".toUpperCase());
+    }
+
+}
