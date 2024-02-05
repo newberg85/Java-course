10770885a8fb42b459993fd448c28a63e607ba39
newberg85 <bergviana12345@gmail.com>
2024-02-05 11:23:08 -0300

    Aula 15 Wrappers

---
+++ src/Aula15Wrapper.java
@@ -0,0 +1,37 @@
+public class Aula15Wrapper {
+
+    public static void main(String[] args) {
+        //byte
+        Byte b = 100;
+        //short
+        Short s = 1000;
+        //int
+        // Integer i = Integer.parseInt("10000");//int
+        Integer i = 10000;
+        //long
+        Long l = 100000L;
+
+        System.out.println(b.byteValue());
+        System.out.println(s.toString());
+        System.out.println(i * 3);
+        System.out.println(l / 3);
+
+        //float
+        Float f = 123.0F;
+        System.out.println(f);
+
+        //double
+        Double d = 1234.5678;
+        System.out.println(d);
+
+        //boolean
+        Boolean bo = Boolean.parseBoolean("true");
+        System.out.println(bo);
+        System.out.println(b.toString().toUpperCase());
+
+        //char
+        Character c = '#';
+        System.out.println(c + "...");
+    }
+
+}
