7e614a4f74e91606db798ffb094e9642188729b9
newberg85 <bergviana12345@gmail.com>
2024-02-06 10:19:31 -0300

    Aula 16 Conversão de tipos primitivos

---
+++ src/Aula16ConversaoDeTiposPrimitivos.java
@@ -0,0 +1,19 @@
+public class Aula16ConversaoDeTiposPrimitivos {
+
+    public static void main(String[] args) {
+        double a = 1; // conversão implicita
+        System.out.println(a);
+
+        float b = (float) 1.12345678; // conversão explicita (CAST)
+        System.out.println(b);
+
+        int c = 4;
+        byte d = (byte) c; // conversão explicita (CAST)
+        System.out.println(d);
+
+        double e = 1.000000;
+        int f = (int) e;
+        System.out.println(f);
+    }
+
+}
