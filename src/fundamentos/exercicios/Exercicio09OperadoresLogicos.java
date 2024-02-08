081362ed5efe3263569a8de71620bd7f4be612ba
newberg85 <bergviana12345@gmail.com>
2024-02-07 23:05:26 -0300

    Exercicio Operadores lógicos

---
+++ src/fundamentos/exercicios/Exercicio09OperadoresLogicos.java
@@ -0,0 +1,14 @@
+package fundamentos.exercicios;
+
+public class Exercicio09OperadoresLogicos {
+
+    public static void main(String[] args) {
+        boolean trabalho1 = true;
+        boolean trabalho2 = true;
+
+        boolean comprouTv50 = trabalho1 && trabalho2;
+
+        System.out.println("Comprou Tv de 50 polegadas\"?" + comprouTv50);
+    }
+
+}
