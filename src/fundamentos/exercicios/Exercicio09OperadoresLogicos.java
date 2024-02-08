371fdc9727b5f168db9310dfdb3a6c3e156367fd
newberg85 <bergviana12345@gmail.com>
2024-02-08 02:06:54 -0300

    Operadores lógicos

--- src/fundamentos/exercicios/Exercicio09OperadoresLogicos.java
+++ src/fundamentos/exercicios/Exercicio09OperadoresLogicos.java
@@ -7,8 +7,15 @@ public class Exercicio09OperadoresLogicos {
         boolean trabalho2 = true;
 
         boolean comprouTv50 = trabalho1 && trabalho2;
+        boolean comprouTv32 = trabalho1 ^ trabalho2;
+        boolean comprouSorvete = trabalho1 || trabalho2;
+        boolean maisSaudavel = !comprouSorvete;
 
         System.out.println("Comprou Tv de 50 polegadas\"?" + comprouTv50);
+        System.out.println("Comprou Tv de 32 polegadas\"?" + comprouTv32);
+        System.out.println("Comprou Tv de 32 polegadas\"Sorvete?" + comprouSorvete);
+        // Operador unário
+        System.out.println("Mais saudavel" + maisSaudavel);
     }
 
 }
