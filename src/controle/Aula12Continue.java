f63b22449e006474ced053092c93ca59eb596e8b
newberg85 <bergviana12345@gmail.com>
2024-02-13 11:31:28 -0300

    Continue

---
+++ src/controle/Aula12Continue.java
@@ -0,0 +1,15 @@
+package controle;
+
+public class Aula12Continue {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            if (i % 2 == 1) {
+                continue;
+            }
+
+            System.out.println(i);
+        }
+    }
+
+}
