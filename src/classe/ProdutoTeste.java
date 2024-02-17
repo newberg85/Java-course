5f4c994eeb83133d2a349a4d20953411270acdfb
newberg85 <bergviana12345@gmail.com>
2024-02-17 11:05:05 -0300

    Classe

---
+++ src/classe/ProdutoTeste.java
@@ -0,0 +1,29 @@
+package classe;
+
+public class ProdutoTeste {
+
+    public static void main(String[] args) {
+
+        Produto p1 = new Produto();
+        p1.nome = "Notebook";
+        p1.preco = 4356.89;
+        p1.desconto = 0.25;
+
+        var p2 = new Produto();
+        p2.nome = "Celular";
+        p2.preco = 2600.85;
+        p2.desconto = 0.29;
+
+        System.out.println(p1.nome);
+        System.out.println(p2.nome);
+
+        double precoFinal1 = p1.preco * (1 - p1.preco);
+        double precoFinal2 = p2.preco * (1 - p2.preco);
+        double mediaDoCarrinho = (precoFinal1 + precoFinal2) / 2;
+
+        System.out.println(precoFinal1);
+        System.out.println(precoFinal2);
+        System.out.printf("Média do carrinho = R$%.2f.", mediaDoCarrinho);
+    }
+
+}
--- src/fundamentos/exercicios/Exercicio01TiposPrimitivos.java
+++ src/fundamentos/exercicios/Exercicio01TiposPrimitivos.java
@@ -3,8 +3,8 @@ public class Exercicio01TiposPrimitivos {
     
     public static void main(String[] args) {
         double nota1 = 9.0;
-        double nota2 = 10.0;
-        double nota3 = 8.0;
+        double nota2 = 7.0;
+        double nota3 = 5.0;
         double media;
 
         media = (nota1 + nota2 + nota3) / 3;
