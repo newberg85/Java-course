e2d5a8ebe0fabec9432c5da234de7ac231b75095
newberg85 <bergviana12345@gmail.com>
2024-02-18 11:24:08 -0300

    Desafio classe Data

---
+++ src/classe/DataTeste.java
@@ -0,0 +1,14 @@
+package classe;
+
+public class DataTeste {
+    
+    public static void main(String[] args) {
+        Data d1 = new Data();
+        d1.dia = 20;
+        d1.mes = 12;
+        d1.ano = 2004;
+
+        System.out.printf("%d/%d/%d", d1.dia, d1.mes, d1.ano);
+    }
+
+}
