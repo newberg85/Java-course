755703a75c5d84aaede3cb8d7ad0d2a148896c6d
newberg85 <bergviana12345@gmail.com>
2024-02-19 02:25:18 -0300

    Construtor 2 desafio

--- src/classe/DataTeste.java
+++ src/classe/DataTeste.java
@@ -4,11 +4,14 @@ public class DataTeste {
     
     public static void main(String[] args) {
         Data d1 = new Data();
-        d1.dia = 20;
-        d1.mes = 12;
-        d1.ano = 2004;
+        // d1.dia = 20;
+        // d1.mes = 12;
+        // d1.ano = 2004;
+
+        Data d2 = new Data(20, 12, 2004);
 
         System.out.println(d1.obterDataFormatada());
+        System.out.println(d2.obterDataFormatada());
     }
 
 }
