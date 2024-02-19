0af078f7513f51ab425e2eb6a0d7a56bf213fcfa
newberg85 <bergviana12345@gmail.com>
2024-02-19 00:40:23 -0300

    método refatorando

--- src/classe/ProdutoTeste.java
+++ src/classe/ProdutoTeste.java
@@ -17,8 +17,8 @@ public class ProdutoTeste {
         System.out.println(p1.nome);
         System.out.println(p2.nome);
 
-        double precoFinal1 = p1.preco * (1 - p1.preco);
-        double precoFinal2 = p2.preco * (1 - p2.preco);
+        double precoFinal1 = p1.precoComDesconto();
+        double precoFinal2 = p2.precoComDesconto();
         double mediaDoCarrinho = (precoFinal1 + precoFinal2) / 2;
 
         System.out.println(precoFinal1);
