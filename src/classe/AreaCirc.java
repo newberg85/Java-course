4b7bfcde063b0883fd997cd45ece2b3e84628888
newberg85 <bergviana12345@gmail.com>
2024-02-22 02:22:26 -0300

    Valor vs referência

--- src/classe/AreaCirc.java
+++ src/classe/AreaCirc.java
@@ -3,7 +3,7 @@ package classe;
 public class AreaCirc {
     double raio;
     // static double pi = 3.14;
-    final static double PI = 3.14; // O Pi não pode mais ser alterado
+    final static double PI = 3.14; // Ao adicionar o final o PI não poderá mais ser alterado
 
     AreaCirc(double raioInicial) {
         raio = raioInicial;
