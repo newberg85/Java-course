20770659e1633639c3246deed06a54dbaeca55ac
newberg85 <bergviana12345@gmail.com>
2024-02-24 01:56:59 -0300

    Array 1

--- src/arrays/Exercicio.java
+++ src/arrays/Exercicio.java
@@ -5,10 +5,11 @@ import java.util.Arrays;
 public class Exercicio {
 
     public static void main(String[] args) {
-        double[] notasAlunoA = new double[3];
+        double[] notasAlunoA = new double[4];
         notasAlunoA[0] = 7.9;
         notasAlunoA[1] = 8;
         notasAlunoA[2] = 6.7;
+        notasAlunoA[3] = 5.7;
 
        System.out.println(Arrays.toString(notasAlunoA));
 
